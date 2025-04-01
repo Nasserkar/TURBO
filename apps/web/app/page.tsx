9d345dfedde475363ba1906bf60b041f0e7f5cee
Nasserkar <nasserk99205@gmail.com>
2025-04-01 22:25:15 +0200

    feat(docs, web): update titles and links for documentation clarity

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read our docs
+            Read the Web
           </a>
         </div>
         <Button appName="web" className={styles.secondary}>
